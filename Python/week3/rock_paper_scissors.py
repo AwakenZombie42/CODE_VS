7e12c6ff5794ceb47a34fe565bc3f7d55a9739bf
AwakenZombie42 <zaidparvez05@gmail.com>
2024-12-24 20:07:05 +0530

    Update Python

--- Python/week3/rock_paper_scissors.py
+++ Python/week3/rock_paper_scissors.py
@@ -16,3 +16,25 @@ def game():
         print("You lose!")
 
 game()
+
+'''
+from random import choice
+
+def game():
+    # Randomly select rock, paper, or scissors for the computer
+    computer = choice(["rock", "paper", "scissors"])
+    # Get the player's input
+    player = input("Rock, Paper, or Scissors? ").lower()
+    print(f"Computer chose: {computer}")
+    # Determine the outcome
+    if computer == player:
+        print("Tie!")
+    elif (computer == "rock" and player == "paper") or \
+         (computer == "paper" and player == "scissors") or \
+         (computer == "scissors" and player == "rock"):
+        print("You win!")
+    else:
+        print("You lose!")
+
+game()
+'''
