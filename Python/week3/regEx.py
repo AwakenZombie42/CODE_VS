e8b133b8d78ba3ab1a9e196ba71c2fcb48fd63a0
AwakenZombie42 <zaidparvez05@gmail.com>
2025-03-11 21:56:31 +0530

    updated Python week3

--- Python/week3/regEx.py
+++ Python/week3/regEx.py
@@ -2,7 +2,7 @@ import re
 
 pattern = '^a...s$'
 test_string = 'abyss'
-resul+t = re.match(pattern, test_string)
+result = re.match(pattern, test_string)
 
 if result:
     print("Search successful")
